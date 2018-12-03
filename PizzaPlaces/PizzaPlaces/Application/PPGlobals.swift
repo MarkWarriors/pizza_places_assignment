8e440d2289498945effde3966aecdff80725b7ca
Marco Guerrieri <marcoguerrieri@gmail.com>
2018-12-03 12:29:54 +0100

    [IMP] Addedd globals for googleMapsKey

---
+++ PizzaPlaces/PizzaPlaces/Application/PPGlobals.swift
@@ -0,0 +1,14 @@
+//
+//  PPGlobals.swift
+//  PizzaPlaces
+//
+//  Created by Marco Guerrieri on 03/12/18.
+//  Copyright © 2018 Marco Guerrieri. All rights reserved.
+//
+
+import Foundation
+
+
+struct PPApiKey {
+    static let googleMapsKey = ""
+}
